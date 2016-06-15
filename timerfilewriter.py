d654062b99979c4045326a912dafc83198d1cf90
hannonevanlinna <hanno.nevanlinna@futurice.com>
2016-06-15 08:30:45 +0300

    timeoutproblem fixed

--- timerfilewriter.py
+++ timerfilewriter.py
@@ -1,5 +1,5 @@
 import time
-import urllib
+import urllib2
 import pickle
 from shutil import copyfile
 
@@ -9,23 +9,25 @@ from shutil import copyfile
 #server url
 serverapi = "https://radiant-taiga-9466.herokuapp.com/api/status"
 #serverapi = "http://localhost:5000/api/status"
+dictstatus = '{"status":0,"time":"3600","timeleft":"0","message":"","changedate":"2016-02-01T16:44:20.153Z"}'
+
 
 while (1):
 	try: 
-		f = urllib.urlopen(serverapi)   
+		f = urllib2.urlopen(serverapi, timeout=5.0)   
                 dictstatus = f.readline()
 	except: 
 		print("network issues")
-	
-
-	if ( len(dictstatus)>10):	
-		try: 
-			file = open('statusr.txt', 'w')
-			pickle.dump(dictstatus, file)
-			file.close()
-			copyfile('statusr.txt', 'status.txt')
-		except:
-			print("ongelma")   
+	else:
+		if ( len(dictstatus)>10):	
+			try: 
+				file = open('statusr.txt', 'w')
+				pickle.dump(dictstatus, file)
+				file.close()
+			except:
+				print("ongelma")
+			else: 
+				copyfile('statusr.txt', 'status.txt')   
 	time.sleep(2.0)
 
 
